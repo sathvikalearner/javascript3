ef45c555fc84b3a217a69957d5e0820e79e8ef52
sathvikalearner <132885965+sathvikalearner@users.noreply.github.com>
2025-01-22 11:38:13 +0530

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,32 @@
+// function main(m,n){
+//     new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if(isNaN(m) || isNaN(n)){
+//                 return reject()
+//             }
+//             for(let i=m;i<=n;i++){
+//                 console.log(i);
+//             }
+//             return resolve()
+//         },5000)
+//     })
+//     .then(() => console.log("Promise is resolved"))
+//     .catch(() => console.log("error occur"))
+// }
+// main(1, 10)
+
+
+
+//BOM-browser object model-to contact to bom we have to go through window
+//DOM-document object model-inside dom we are having root element which is document,inside document all html code will be stored in tree kind of structure.
+//html divided into 2 parts one is head and another is body.inside head we are having meta tag & title tag.inside body tag all the tags are present
+//DOM-->document-->html(head,body)
+//each tag in DOM called as node
+// DOM is an object.it is an api provided by window.
+//DOM is having root element called document.DOM is having life span(page reloads)
+//purpose of DOM is used to create ,delete and update a element
+//it is used to provide style for an element
+
+//how to convert html structure into DOM tree?
+//
+
