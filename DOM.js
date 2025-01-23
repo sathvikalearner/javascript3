59a23aa5e42f8db597c2b1cf7803677ee3800f8c
sathvikalearner <132885965+sathvikalearner@users.noreply.github.com>
2025-01-23 11:37:02 +0530

    Add files via upload

--- DOM.js
+++ DOM.js
@@ -28,5 +28,44 @@
 //it is used to provide style for an element
 
 //how to convert html structure into DOM tree?
-//
 
+// console.log(window);
+// console.log(document);
+// console.log(document.URL);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.title);
+// document.title = "amazon"
+// console.log(document.body);
+
+// console.log(window);
+// console.log(document);
+// console.log(document.URL);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.title);
+// document.title = "amazon"
+// console.log(document.body);
+
+
+// let image = document.images
+// console.log(img);
+// // console.log(img[0]);
+// // console.log(img[1]);
+// // console.log(img[2]);
+
+// for(let i=0; i<=img.length; i++){
+//     console.log(img[i]);
+//     img[i].style.height = "200px"
+//     img[i].style.width = "200px"
+//     img[i].style.borderRadius = "100%"
+//     img[i].alt = "image"
+// }
+
+//in-direct access in dom (methods in dom)
+// 1) document. getElementById("value")
+// 2) document getElementsByClassName ("value")
+// 3) document getElementsByTagName ("value")
+// 4) document. getElementsByName("value")
+// 5) document. querySelector ("value")
+// 6) document. querySelectorA11("value")
