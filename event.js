f7f5e6e338a4dfeec8d875eecd5b887b7015b682
sathvikalearner <132885965+sathvikalearner@users.noreply.github.com>
2025-01-25 19:07:44 +0530

    Add files via upload

--- event.js
+++ event.js
@@ -1,34 +1,55 @@
-//events in dom
-//--> it is an action performed by end-user on a web-page
-//event is an action performed by enduser on a webpage
-//event types:keyboardevent,mouse event,form event and pointer event
-//keyboard-keypress,keydown,keyup .again having keyup,keydown,keypress
-//mouse event-onmouseover,onmouseleave,dbclick-doubleclick
-//form event-inpu event,focus event,blur event
-//pointer-click event
+// let btn = document.getElementById("demo")
+// console.log(btn);
+
+// btn.onclick = function(e){
+//     // Math.random() * 10000)
+//     console.log(e);
+
+//     let res = Math.round(Math.random() * 10000).toString(16)
+//     console.log(res);
+//     document.body.style.backgroundColor = `#${res}`
+// }
+
+// let btn1 = document.getElementById("demo1")
+// console.log(btn1);
+
+// let color = window.prompt("enter any color name")
+// console.log(color);
+
+// btn1.innerHTML = color
+
+// btn1.onmouseover = () => {
+//     document.body.style.backgroundColor = `${color}`
+//     document.body.style.transitionDuration="3s"
+// }
 
 
-//-->types
+// let video = document.getElementById("demo2")
+// console.log(video);
 
-//--> 1. key-board event
-         //key-up
-         //key-down
-         //key-press
+// video.addEventListener("click", () =>{
+//     let res = video.classList.toggle("demo")
+//     if(res)
+//     {
+//         video.play()
+//     }else{
+//         video.pause()
+//     }
+// })
+// toggle will act like a boolean  .
+// toggle helps to execute video like pause and play for first click the video will play for send click the videp will pause same as yt .
+// toggle can exectue in TFTFTF manner
 
-//--> 2. mouse event
-         //onmouseover
-         //onmouseleave
-         //dbclick
-         
-//--> 3. form event
-         //input
-         //focus
-         //key-press
+let demo4 = document.getElementById("demo4")
+console.log("demo4")
 
-//--> 4. pointer event
-        //click-event
+demo4.addEventListener("click",() => {
+    let demo3 = document.getElementById("demo3").value
+    console.log("demo3");
 
+    let speech = new SpeechSynthesisUtterance
+    console.log("speech");
+    speech.text = demo3
+    speechSynthesis.speak("speech")
 
-        function event1(){
-            console.log("hello-world")
-        }
+})
