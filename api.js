bd2786a8dfb4a2e46b1bc2060fb6dc347b6d22cc
sathvikalearner <132885965+sathvikalearner@users.noreply.github.com>
2025-01-29 14:38:11 +0530

    Add files via upload

--- api.js
+++ api.js
@@ -41,7 +41,7 @@
 // 1.fetch() method
 // 2.ajax
 
-//fetch present in windowm 
+// fetch present in windowm 
 // let a =windows.fetch()
 // console.log(a)
 
@@ -56,20 +56,51 @@
 //     res.map((x) => {
 //         console.log(x)
 //         console.log(x.name)
+//         console.log(x.id)
+
+//         document.body.innerHTML  += `<h1>${x.name}</h1>`
+//         document.body.innerHTML  += `<h2>${x.id}</h2>`
+//         document.body.innerHTML  += `<h2>${x.designation}</h2>`
+
     
 //     })
 // })
 
+// when one then method works properly we can use another then method and it is called promise chaining
 
 
-async () => {
-    let data = await fetch("data.json")
-    console.log(data)
-    let res = await data.json()
-    console.log(res)
 
-    res.map((res) => {
+// async () => {
+//     let data = await fetch("data.json")
+//     console.log(data)
+//     let res = await data.json()
+//     console.log(res)
+
+//     res.map((res) => {
+//         console.log(res);
+//         document.body.innerHTML  += <h1>${res.name}</h1>
+//     })
+// }
+
+let btn = document.getElementById("btn")
+console.log(btn);
+
+btn.onclick = () => {
+    let search = document.getElementById("search").value 
+    console.log(search);
+    let page = 10
+    window.fetch(`https://pixabay.com/api/?key=36434946-acc22cf6e0589aeef704c8b24&q=${search}&image_type=photo&pretty=true&per_page=${page}`)
+    .then((data) => data.json())
+    .then((res) => {
         console.log(res);
-        document.body.innerHTML  += <h1>${res.name</h1>
+        console.log(res.hits);
+        res.hits.map((x) => {
+            console.log(x)
+
+            let demo = document.getElementById("demo")
+            console.log(demo)
+            demo.innerHTML +=`<img src = ${x.previewURL} />`
+        })
     })
 }
+
